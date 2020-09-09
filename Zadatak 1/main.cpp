54dd6981eb14c21f676353cccc7e93c30ba52cb5
ahmedjasarevic <66922656+ahmedjasarevic@users.noreply.github.com>
2020-09-09 20:30:20 +0200

    zadatak 1.2

--- Zadatak 1/main.cpp
+++ Zadatak 1/main.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 
 using namespace std;
-int kvadar (int duzina, int sirina, int dubina, int sirinaplocice){
-int dno = duzina * sirina;
-int lijevaidesna = sirina * dubina;
-int lijevaidesna2 = sirina * dubina;
-int gornjaidonja = duzina * dubina;
-int gornjaidonja2 = duzina * dubina;
-return  (dno + lijevaidesna + gornjaidonja + lijevaidesna2 + gornjaidonja2) * sirinaplocice * sirinaplocice;
 
 
+int povrsinaBazena (int duzina, int sirina, int dubina){
+    int dno = duzina * sirina;
+    int lijevaidesna = sirina * dubina;
+    int gornjaidonja = duzina * dubina;
+    return  (dno + lijevaidesna*2 + gornjaidonja*2);
 }
+
+
+double pretvoriCentimetar2uMetar2 (int stranica){
+    return stranica*stranica/10000.0;
+}
+
+
 int main()
 {
     int duzina, sirina, dubina;
@@ -19,7 +24,13 @@ int main()
     std::cin >> duzina >> sirina >> dubina;
     std::cout << "Unesite sirinu plocice u centimetrima: " ;
     std::cin >> sirinaplocice;
+    pretvoriCentimetar2uMetar2(sirinaplocice);
+    double f = povrsinaBazena(duzina,sirina,dubina) / pretvoriCentimetar2uMetar2(sirinaplocice);
+    if (f - (int)f > 0){
+    std::cout << "Poplocavanje bazena dimenzija  " << duzina <<"x" << sirina << "x" << dubina <<"m" << " sa plocicama dimenzija "
+    << sirinaplocice << "x" << sirinaplocice << " cm " << " nije izvodljivo bez lomljenja plocica!"; }
+    else {
     std::cout << "Za poplocavanje bazena dimenzija " << duzina <<"x" << sirina << "x" << dubina <<"m" << " sa plocicama dimenzija "
-    << sirinaplocice << "x" << sirinaplocice << " cm" << " potrebno je " <<  kvadar(duzina,sirina,dubina,sirinaplocice) << " plocica" ;
+    << sirinaplocice << "x" << sirinaplocice << " cm " << " potrebno je " <<  f << " plocica" ; }
     return 0;
 }
