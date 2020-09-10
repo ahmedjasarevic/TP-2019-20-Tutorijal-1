d967784b4e1d92d9e8c6718fcaa7c4788821411b
ahmedjasarevic <66922656+ahmedjasarevic@users.noreply.github.com>
2020-09-10 21:56:11 +0200

    Zadatak 3.

--- Zadatak 3/Zadatak 3/main.cpp
+++ Zadatak 3/Zadatak 3/main.cpp
@@ -2,32 +2,22 @@
 #include <cmath>
 #include <iomanip>
 
-const int velicina=9;
-using namespace std;
-double korijenuj (int a){
-return sqrt(a);
 
-}
-int kvadriraj (int a){
-return a*a;
-}
+using namespace std;
+
+
 int main()
 {
     int broj1,broj2;
     cout << "Unesite pocetnu i krajnu vrijednost:" << endl;
     cin >> broj1 >> broj2;
-    cout << "Brojevi" << endl;
-    for (int i=broj1; i<=broj2; i++){
-        cout<<setw(3)<<i<<" ";
-    cout<<endl;}
-     cout << "Kvadrati" << endl;
-    for (int i=broj1; i<=broj2; i++){
-        cout << kvadriraj(i) << endl;
-        }
-
-    cout << "Korijeni" << endl;
-    for (int i=broj1; i<=broj2; i++){
-        cout << korijenuj(i) << endl;
-    }
+    cout << "+---------+----------+----------+-----------+" << endl;
+    cout << "| Brojevi | Kvadrati | Korijeni | Logaritmi |" << endl;
+    cout << "+---------+----------+----------+-----------+"<<endl;
+    for(int i = broj1; i <= broj2; i++){
+    cout<<"| " << setw(8) << left << i << "|" << setw(9) << right << i*i << " |" << setw(9) << right <<
+    fixed << setprecision(3) << sqrt(i) << " |" << setw(10) << right <<fixed <<  setprecision(5) << log(i) << " |" << endl;
+}
+    cout << "+---------+----------+----------+-----------+" << endl;
     return 0;
 }
