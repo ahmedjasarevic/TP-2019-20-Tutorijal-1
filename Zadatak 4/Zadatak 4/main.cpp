847e742d1ef6a5db3eaaae28d6522137cf852858
ahmedjasarevic <66922656+ahmedjasarevic@users.noreply.github.com>
2020-09-10 15:16:51 +0200

    Zadatak 4

--- Zadatak 4/Zadatak 4/main.cpp
+++ Zadatak 4/Zadatak 4/main.cpp
@@ -1,35 +1,37 @@
 #include <iostream>
-
 using namespace std;
 
-int main(){
 
-int broj1;
-int suma=0;
-do {
-cout << "Unesite prirodan broj ili 0 za kraj: ";
-cin >> broj1;
-if (broj1 == 0)
-            break;
-if (broj1 < 0){
-    cout << "Niste unijeli prirodan broj!";
-        return 0;
-    }
-for (int i=1; i<broj1; i++){
-if (broj1 % i == 0){
-    suma=suma+i;
-}
-}
- if (suma < broj1){
-    cout << "Broj " <<broj1 <<" je manjkav" << endl;
- }
- else if (suma > broj1){
-    cout << "Broj " <<broj1 <<" je obilan"<< endl;
- }
- else if (suma == broj1){
-    cout << "Broj " <<broj1 <<" je savrsen"<< endl;
- }
-} while (broj1 != 0);
-return 0;
+int main(){
+    int broj1;
+    int suma = 0;
+
+    do {
+        cout << "Unesite prirodan broj ili 0 za kraj: ";
+        cin >> broj1;
+        if (broj1 < 0 || !cin){
+            cout << "Niste unijeli prirodan broj!";
+            continue;
+        }
+        else if (broj1 == 0)
+                break;
+
+        for (int i=1; i<broj1; i++){
+            if (broj1 % i == 0)
+                suma+=i;
+        }
+
+        if (suma < broj1){
+            cout << "Broj " <<broj1 <<" je manjkav" << endl;
+        }
+        else if (suma > broj1){
+            cout << "Broj " <<broj1 <<" je obilan"<< endl;
+        }
+        else if (suma == broj1){
+            cout << "Broj " <<broj1 <<" je savrsen"<< endl;
+        }
+        suma = 0;
+    } while (broj1 != 0);
+    return 0;
 }
 
